fa6e6bfdb3e028eb8ce3fcd2046548bf105c13a1
kvlknctk <iletisim@volkancatak.com.tr>
2019-03-22 18:11:25 +0300

    Exit to dark theme.

--- resources/views/calismalar_detail.blade.php
+++ resources/views/calismalar_detail.blade.php
@@ -6,7 +6,7 @@
 
     @include('part.breadcumb')
 
-    <section id="content" aria-label="section-blogg">
+    <section id="content" aria-label="section-blogg" class="whitepage">
         <div class="container">
             <div class="row">
 
